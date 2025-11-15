753f239c031f9c397e9e5b9f7faae11b6dadbb0d
Alan Malbos <alanmalbos@yahoo.com.br>
2025-11-15 09:39:41 -0300

    fix: app

--- config/app.php
+++ config/app.php
@@ -51,7 +51,7 @@ return [
     'App' => [
         'namespace' => 'App',
         'encoding' => env('APP_ENCODING', 'UTF-8'),
-        'defaultLocale' => env('APP_DEFAULT_LOCALE', 'pt_BR'),
+        'defaultLocale' => env('APP_DEFAULT_LOCALE', 'en_US'),
         'defaultTimezone' => env('APP_DEFAULT_TIMEZONE', 'America/Sao_Paulo'),
         'base' => false,
         'dir' => 'src',
